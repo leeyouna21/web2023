ef89573a1c723e97d90d060c9f83d5a4c9e9c965
leeyouna21 <yuna5845@gmail.com>
2023-06-01 16:37:33 +0900

    1

--- site03/site2/src/App.js
+++ site03/site2/src/App.js
@@ -1,32 +1,26 @@
 import React from 'react';
-import Header from './components/layout/Header';
-import Main from './components/layout/Main';
-import SliderSection from './components/session/SliderSection';
-import IntroSection from './components/session/IntroSection';
-import MemberSection from './components/session/MemberSection';
-import PortSection from './components/session/PortSection';
-import YoutubeSection from './components/session/YoutubeSection';
-import UnsplashSection from './components/session/UnsplashSection';
-import MovieSection from './components/session/MovieSection';
-import ReviewSection from './components/session/ReviewSection';
-import Footer from './components/layout/FooterSection';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './components/pages/Main';
+import Intro from './components/pages/Intro';
+import Member from './components/pages/Member';
+import Port from './components/pages/Port.jsx';
+import Youtube from './components/pages/Youtube';
+import Unsplash from './components/pages/Unsplash';
+import Movie from './components/pages/Movie';
 
 function App() {
   return (
-    <>
-      <Header attr={'header__wrap SBAggro3 bg-beige'} />
-      <Main>
-        <SliderSection attr={'slider__wrap SBAggro3 section '} />
-        <IntroSection attr={'intro__wrap SBAggro3  section bg-ivory'} />
-        <MemberSection attr={'member__wrap SBAggro3 center section'} />
-        <PortSection attr={'port__wrap SBAggro3  center section bg-ivory'} />
-        <YoutubeSection attr={'youtube__wrap SBAggro3  section  bg-white02'} />
-        <UnsplashSection attr={'unsplash__wrap SBAggro3 section bg-white'} />
-        <MovieSection attr={'movies__wrap SBAggro3 section bg-white02'} />
-        <Footer attr={'footer__wrap SBAggro3 section bg-white02'} />
-      </Main>
-      <ReviewSection />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/intro" element={<Intro />} />
+        <Route path="/member" element={<Member />} />
+        <Route path="/port" element={<Port />} />
+        <Route path="/youtube" element={<Youtube />} />
+        <Route path="/unsplash" element={<Unsplash />} />
+        <Route path="/movie" element={<Movie />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ site03/site2/src/components/pages/Unsplash.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Unsplash() {
+  return <div>Unsplash</div>;
+}
+
+export default Unsplash;
